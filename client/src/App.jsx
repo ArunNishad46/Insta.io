396bbbd166433b2a5b5a5b31f970b8503b904809
Arun <157214616+ArunNishad46@users.noreply.github.com>
2026-09-11 13:59:42 +0530

    Improve performance of app
    Removed loading state check from App component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import AppRouter from './router/AppRouter.jsx';
 import Navbar from './components/Navbar.jsx';
-import { useSelector } from 'react-redux';
 
 export default function App() {
-  const { initialized } = useSelector((state) => state.auth);
-
-  if (!initialized) return <div className='pt-15 text-center text-purple-700 text-lg font-semibold'>Loading...</div>;
-
   return (
     <div className="bg-gray-50 min-h-screen">
       <Navbar />
