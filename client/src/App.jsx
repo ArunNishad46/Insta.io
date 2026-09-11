40e30c46375691f444fce2a2a2ca7ba73ef2df8c
Arun <157214616+ArunNishad46@users.noreply.github.com>
2026-09-11 15:06:46 +0530

    Implement loading state in App component
    Add loading state for app initialization using Redux.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppRouter from './router/AppRouter.jsx';
 import Navbar from './components/Navbar.jsx';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+  const { initialized } = useSelector((state) => state.auth);
+
+  if (!initialized) return <div className='pt-15 text-center text-purple-700 text-lg font-semibold'>Loading...</div>;
+
   return (
     <div className="bg-gray-50 min-h-screen">
       <Navbar />
